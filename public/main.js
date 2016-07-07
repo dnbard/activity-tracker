e875f0246e77c0faf45e1a5d2f627e66357e9725
Oleksandr Bardanov <oleksandr.bardanov@avid.com>
2016-07-07 15:54:07 +0300

    report stats and fetch them back

--- public/main.js
+++ public/main.js
@@ -2,6 +2,7 @@ const token = localStorage.getItem('__token');
 
 if (!token){
     throw new Error('Token should be defined in LocalStorage!');
+    location.href = '/login.html';
 }
 
 const application = document.querySelector('main');
@@ -66,38 +67,110 @@ for(var weekIndex = 1; weekIndex < 53; weekIndex ++){
 
 let reportsCollection = [];
 
+const colors = {
+    5: '#1e6823',
+    4: '#44a340',
+    3: '#8cc665',
+    2: '#d6e685',
+    1: '#f9e58f'
+};
+
+function colorCell(report){
+    let date = new Date(report.timestamp);
+    date.setHours(0, 0, 0, 0);
+
+    const el = document.querySelector(`.day[data-timestamp="${date}"]`);
+    el._report = report;
+
+    if (report.score > 0){
+        el.style.background = colors[report.score];
+    }
+}
+
 fetch('/reports', {
     headers: { authorization: token }
 }).then(r => r.json()).then(reports => {
-    reports.forEach(r => {
-        let date = new Date(r.timestamp);
-        date.setHours(0, 0, 0, 0);
-
-        const el = document.querySelector(`.day[data-timestamp="${date}"]`);
-        el._report = r;
-
-        if (r.score > 0){
-            el.style.background = 'green';
-        }
-    });
+    reports.forEach(colorCell);
 
     reportsCollection = reports;
 });
 
 
 const explorerTemplate = Handlebars.compile(document.querySelector("#explorer-template").innerHTML);
+const explorerReportElement = document.querySelector("#explorer-report");
+
 weeksWrapper.onclick = function(e){
     const element = document.querySelector('#explorer');
+    const report = e.target._report;
 
-    if (e.target.className !== 'day' || !e.target._report){
+    if (e.target.className !== 'day'){
         element.innerHTML = '';
+        explorerReportElement.style.display="none";
         return;
     }
 
-    const report = e.target._report;
+    if (!report){
+        element.innerHTML = '';
+        explorerReportElement.style.display="block";
+        explorerReportElement.querySelector('.date').textContent = e.target.title;
+        explorerReportElement._date = e.target.getAttribute('data-timestamp');
+        starSelected = -1;
+        highlightStars(-1);
+        return;
+    }
 
     element.innerHTML = explorerTemplate({
         date: new Date(report.timestamp).toDateString(),
         score: report.score
+    });
+    explorerReportElement.style.display="none";
+}
+
+let starSelected = -1;
+function highlightStars(hoverIndex){
+    for (var i = 0; i < 5; i ++){
+        explorerReportElement.querySelector(`[data-index="${i}"]`).className =
+            i <= Math.max(starSelected, hoverIndex) ? "large material-icons black" : "large material-icons gray";
+    }
+}
+
+explorerReportElement.querySelector('.score').onmouseover = (e) => {
+    if (e.target.className.indexOf('material-icons') === -1){
+        highlightStars(-1);
+        return;
+    }
+
+    const starIndex = parseInt(e.target.getAttribute('data-index'));
+    highlightStars(starIndex);
+}
+
+explorerReportElement.querySelector('.score').onclick = function(e){
+    if (e.target.className.indexOf('material-icons') === -1){
+        return;
+    }
+
+    const starIndex = parseInt(e.target.getAttribute('data-index'));
+    starSelected = starIndex;
+    highlightStars(-1);
+}
+
+explorerReportElement.querySelector('.btn.send').onclick = function(e){
+    if (starSelected < 0){
+        return;
+    }
+
+    fetch('/reports', {
+        method: 'post',
+        headers: {
+            authorization: token,
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify({
+            score: starSelected + 1,
+            timestamp: explorerReportElement._date
+        })
+    }).then(r => r.json()).then((report) => {
+        explorerReportElement.style.display="none";
+        colorCell(report);
     });
 }
