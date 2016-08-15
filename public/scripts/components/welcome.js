55311dbad6ab00afbac3e7cc71fb5a036f96d8e5
Oleksandr Bardanov <oleksandr.bardanov@avid.com>
2016-08-15 14:58:17 +0300

    To implement changes for header, close #10

--- public/scripts/components/welcome.js
+++ public/scripts/components/welcome.js
@@ -5,7 +5,13 @@ define([
         init: function(options){
             fetch('/users').then(user => {
                 const welcomeSection = document.querySelector(options.selector);
-                welcomeSection.textContent = `Hello, ${user.email}.`;
+                const template = Handlebars.compile(document.querySelector("#welcome-template").innerHTML);
+                welcomeSection.innerHTML = template({ person: user.email });
+
+                welcomeSection.querySelector('.btn-logout').onclick = () => {
+                    localStorage.setItem('__token', '');
+                    location.href = '/login.html';
+                }
             });
         }
     }
